26c251dc692310ffb67022c3ce9806ebff694063
Ronnie <13062058+ronnievdc@users.noreply.github.com>
2022-07-11 23:03:46 +0200

    0.4.1

--- GearOverview.lua
+++ GearOverview.lua
@@ -3,7 +3,7 @@ local lib = GearOverview
 
 lib.name = "GearOverview"
 lib.author = "@ronnievdc"
-lib.version = "0.3.3"
+lib.version = "0.4.0"
 lib.setList = {}
 lib.presets = {}
 lib.activePreset = nil
